bb87e205476161e1ae14572ba16341b273230639
rachelbookhout <rbookhou@gmail.com>
2014-11-12 15:36:57 -0500

    Debugged, added details about books

--- js/app.js
+++ js/app.js
@@ -20,7 +20,6 @@ App.Book = DS.Model.extend({
   review: DS.attr(),
   rating: DS.attr('number'),
   amazon_id: DS.attr(),
-  genre: DS.belongsTo('genre'),
   url: function() {
     return "http://www.amazon.com/gp/product/"+this.get('amazon_id')+"/adamfortuna-20";
   }.property('amazon_id'),
@@ -37,7 +36,6 @@ App.Book.FIXTURES = [
     review: 'Although this book focuses on the cognitive advantages to having children use technology from an early age, it is also an in depth look at how people can learn for themseves. As someone who was often distracted and bored at times during school, Mindstorms highlights some of the reasoning behind that feeling and what we can do as teachers to help minimize it.',
     rating: 5,
     amazon_id: '0465046746',
-    genre: 3
   },
   {
     id: 2,
@@ -46,7 +44,6 @@ App.Book.FIXTURES = [
     review: "Probably my favorite science fiction book (and series) I've ever read. Hyperion is written in a style similar to The Canterbury Tales, in which a series of stories are told by the main characters. Each story is a gem in itself, but alude to the larger storyline. The scope of the story is ambitious - spanning time, planets religion and love.",
     rating: 5,
     amazon_id: '0553283685',
-    genre: 1
   },
   {
     id: 3,
@@ -55,6 +52,5 @@ App.Book.FIXTURES = [
     review: "Even though I respect Ive, I felt this biography only hit skin deep. It went over all the major events in his life, his passion for design, awards he achieved -- but that's really it. I dont't feel I know him anymore than before reading this.",
     rating: 2,
     amazon_id: '159184617X',
-    genre: 3
   }
 ];
